cc05fbb19f97d80bd35ce82cb262aa5ce7ced9b0
Choyz <nagatoyuki1024@gmail.com>
2022-06-23 16:22:08 +0800

    feat: add auth system

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const path = require('path')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const methodOverride = require('method-override')
+const usePassport = require('./config/passport')
+const flash = require('connect-flash')
 const routes = require('./routes')
 
 if (process.env.NODE_ENV !== 'production') {
@@ -18,8 +21,28 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+)
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
+
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 
 app.use(routes)
 
--- src/routes/modules/users.js
+++ src/routes/modules/users.js
@@ -1,10 +1,37 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 const User = require('../../models/User')
 
 router.get('/login', (req, res) => {
-  res.render('login')
+  res.render('login', {
+    error: req.flash('error'),
+  })
+})
+
+router.post(
+  '/login',
+  (req, res, next) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+      req.flash('warning_msg', 'Please enter Email & Password.')
+      return res.redirect('/users/login')
+    }
+    return next()
+  },
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login',
+    failureFlash: true,
+  })
+)
+
+router.get('/logout', (req, res) => {
+  req.logout()
+  req.flash('success_msg', 'You have been successfully logged out.')
+  res.redirect('/users/login')
 })
 
 router.get('/register', (req, res) => {
@@ -13,24 +40,43 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: 'All fields are required.' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: 'The password confirmation does not match!' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+    })
+  }
   User.findOne({ email })
     .then((user) => {
       if (user) {
-        console.log('User already exists.')
-        res.render('register', {
+        errors.push({ message: 'This email is already being used.' })
+        return res.render('register', {
+          errors,
           name,
           email,
         })
-      } else {
-        return User.create({
-          name,
-          email,
-          password,
-          type: 'email',
-        })
-          .then(() => res.redirect('/'))
-          .catch((err) => console.log(err))
       }
+      return bcrypt
+        .genSalt(10)
+        .then((salt) => bcrypt.hash(password, salt))
+        .then((hash) =>
+          User.create({
+            name,
+            email,
+            password: hash,
+            type: 'email',
+          })
+        )
+        .then(() => res.redirect('/'))
+        .catch((err) => console.log(err))
     })
     .catch((err) => console.log(err))
 })
