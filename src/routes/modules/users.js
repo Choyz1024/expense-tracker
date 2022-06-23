b401e5bb37522fe73522a81582d36b9e05c5f6b0
choyz1024 <nagatoyuki1024@gmail.com>
2022-06-23 11:13:10 +0800

    feat: add user model

--- src/routes/modules/users.js
+++ src/routes/modules/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/User')
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -9,5 +11,28 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        console.log('User already exists.')
+        res.render('register', {
+          name,
+          email,
+        })
+      } else {
+        return User.create({
+          name,
+          email,
+          password,
+          type: 'email',
+        })
+          .then(() => res.redirect('/'))
+          .catch((err) => console.log(err))
+      }
+    })
+    .catch((err) => console.log(err))
+})
 
 module.exports = router
